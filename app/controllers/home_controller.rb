c0ef06faff0cd4e87f761bf49db7fe5385b54d56
hc5duke <hc5duke@gmail.com>
2012-04-20 10:09:28 -0700

    per tile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,25 @@ class HomeController < ApplicationController
     image = Magick::Image.from_blob(params[:picture].read).first
     image = image.quantize(16, Magick::GRAYColorspace)
     @images = []
-    15.times.each do |col|
-      top = 65 + (col * 320 / 15).to_i
-      height = (320 / 15).to_i
-      img = image.crop(0, top, 320, height)
-      @images << ActiveSupport::Base64.encode64(img.to_blob)
+    screen_width = 640
+    screen_height = 960
+    offset_top = 130
+    num_tiles = 15
+    tile_offset_left = 4
+    tile_offset_right = 8
+    tile_offset_y = 8
+    tile_size = (screen_width / num_tiles).to_i
+    tile_width = tile_size - tile_offset_left - tile_offset_right
+    tile_height = tile_size - 2 * tile_offset_y
+    num_tiles.times.each do |col|
+      top = offset_top + col * screen_width / num_tiles
+      arr = num_tiles.times.map do |row|
+        left = row * screen_width / num_tiles
+        img = image.resize(screen_width, screen_height).
+          crop(left.to_i + tile_offset_left, top.to_i + tile_offset_y, tile_width, tile_height)
+        Base64.encode64(img.to_blob)
+      end
+      @images << arr
     end
   end
 end
