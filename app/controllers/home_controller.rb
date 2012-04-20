85f6e4aebce20c7d89262f463ecf5ba6618120d7
hc5duke <hc5duke@gmail.com>
2012-04-20 09:31:07 -0700

    by col

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,15 @@ class HomeController < ApplicationController
   end
 
   def analyze
+    params[:picture]
+    image = Magick::Image.from_blob(params[:picture].read).first
+    image = image.quantize(16, Magick::GRAYColorspace)
+    @images = []
+    15.times.each do |col|
+      top = 65 + (col * 320 / 15).to_i
+      height = (320 / 15).to_i
+      img = image.crop(0, top, 320, height)
+      @images << ActiveSupport::Base64.encode64(img.to_blob)
+    end
   end
 end
