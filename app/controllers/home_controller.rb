469759ce5a1a1f5c7476ff5e5cb252b3ec72a26e
hc5duke <hc5duke@gmail.com>
2012-04-21 22:19:45 -0700

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
     num_tiles = 15
     tile_offset_left = 4
     tile_offset_right = 8
-    tile_offset_y = 8
+    tile_offset_y = 6
     tile_size = (screen_width / num_tiles).to_i
     tile_width = tile_size - tile_offset_left - tile_offset_right
     tile_height = tile_size - 2 * tile_offset_y
@@ -28,27 +28,13 @@ class HomeController < ApplicationController
         img = image.crop(left.to_i, top.to_i, tile_width, tile_height).quantize(2, Magick::HSLColorspace)
         color_histogram = img.color_histogram
         palette = color_histogram.reject { |key, val| val < 100 }
-        if palette.length == 1
-          cols << 'empty'
-        elsif color_histogram.select { |key, val| key.red > 40000 && key.blue < 40000 && key.green < 40000 }.length > 0
-          cols << 'dw'
-        elsif color_histogram.select { |key, val| key.red < 40000 && key.blue > 40000 && key.green < 40000 }.length > 0
-          cols << 'dl'
-        elsif color_histogram.select { |key, val| key.red < 40000 && key.blue < 40000 && key.green > 40000 }.length > 0
-          cols << 'tl'
-        else
-          diff = img.difference(tw_tile)
-          if diff[0] < 4000 && diff[1] < 0.04
-            cols << 'tw'
-          else
-            cols << palette
-          end
-        end
+        palette = 'empty' if palette.length == 1
+        cols << palette
         if color_histogram.select { |key, val| key.red > 50000 && key.blue > 50000 && key.green > 50000 }.length > 0
-          Base64.encode64(img.negate.charcoal.to_blob)
-        else
-          Base64.encode64(img.charcoal.to_blob)
+          img = img.negate
         end
+        img = img.charcoal.resize(18,18).quantize(2, Magick::GRAYColorspace)
+        Base64.encode64(img.to_blob)
       end
       @colors << cols
     end
