3f22c8903cc3423dd094f12d3923f9f8a9d9b6a4
hc5duke <hc5duke@gmail.com>
2012-04-22 00:14:33 -0700

    still kinda slow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,17 @@ class HomeController < ApplicationController
   end
 
   def analyze
-    tw_tile = Magick::Image::read('./data/tw.gif').first
     screen_width = 640
     screen_height = 960
     offset_top = 130
     num_tiles = 15
-    tile_offset_left = 4
-    tile_offset_right = 8
-    tile_offset_y = 6
+    tile_offset_left = 6
+    tile_offset_right = 12
+    tile_offset_top = 9
+    tile_offset_bottom = 9
     tile_size = (screen_width / num_tiles).to_i
     tile_width = tile_size - tile_offset_left - tile_offset_right
-    tile_height = tile_size - 2 * tile_offset_y
+    tile_height = tile_size - tile_offset_top - tile_offset_bottom
     colorspace = 3
 
     image = Magick::Image.from_blob(params[:picture].read).first.
@@ -21,20 +21,21 @@ class HomeController < ApplicationController
     @images = []
     @colors = []
     num_tiles.times.each do |col|
-      top = offset_top + col * screen_width / num_tiles + tile_offset_y
+      top = offset_top + col * screen_width / num_tiles + tile_offset_top
       cols = []
       @images << num_tiles.times.map do |row|
         left = row * screen_width / num_tiles + tile_offset_left
-        img = image.crop(left.to_i, top.to_i, tile_width, tile_height).quantize(2, Magick::HSLColorspace)
+        tile = image.crop(left.to_i, top.to_i, tile_width, tile_height)
+        img = tile.quantize(2, Magick::HSLColorspace)
         color_histogram = img.color_histogram
         palette = color_histogram.reject { |key, val| val < 100 }
-        palette = 'empty' if palette.length == 1
-        cols << palette
-        if color_histogram.select { |key, val| key.red > 50000 && key.blue > 50000 && key.green > 50000 }.length > 0
-          img = img.negate
+        if palette.length == 1
+          cols << 'empty'
+        else
+          cols << palette
+          img = img.negate if color_histogram.select { |key, val| key.red > 50000 && key.blue > 50000 && key.green > 50000 }.length > 0
+          Base64.encode64(img.charcoal.quantize(2, Magick::GRAYColorspace).to_blob)
         end
-        img = img.charcoal.resize(18,18).quantize(2, Magick::GRAYColorspace)
-        Base64.encode64(img.to_blob)
       end
       @colors << cols
     end
