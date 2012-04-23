259232ac5e22bebf63a4405fc778c262fed8d734
hc5duke <hc5duke@gmail.com>
2012-04-23 00:53:26 -0700

    good

--- lib/recolor.rb
+++ lib/recolor.rb
@@ -1,7 +1,7 @@
 require 'rmagick'
 
-color = Magick::Pixel.new(9000, 9000, 9000, 65535)
-color = Magick::Pixel.new(0, 0, 0, 65535)
+color_white = Magick::Pixel.new(65535, 65535, 65535, 65535)
+color_black = Magick::Pixel.new(0, 0, 0, 65535)
 Dir['*.png'].each do |name|
   next if /black/ === name
   black = Magick::Image.read("./#{name}").first
@@ -10,13 +10,30 @@ Dir['*.png'].each do |name|
   min = black.export_pixels(0, 0, 24, 14, 'I').min
   if min > 20_000
     24.times.each do |col|
-      pixels = black.export_pixels(0, col, 24, 1, 'I')
-      max = pixels.max
-      if max > 60_000
-        index = pixels.index(max) + col * 24
-        col = index % 24
-        row = index / 24
-        black = black.color_floodfill(col, row, color)
+      black.export_pixels(0, col, 24, 1, 'I').each_with_index do |pixel, index|
+        if pixel > 60_000
+          color = color_black
+        else
+          color = color_white
+        end
+        index += col * 24
+        co = index % 24
+        ro = index / 24
+        black = black.color_point(co, ro, color)
+      end
+    end
+  else
+    24.times.each do |col|
+      black.export_pixels(0, col, 24, 1, 'I').each_with_index do |pixel, index|
+        if pixel < 20_000
+          color = color_black
+        else
+          color = color_white
+        end
+        index += col * 24
+        co = index % 24
+        ro = index / 24
+        black = black.color_point(co, ro, color)
       end
     end
   end
