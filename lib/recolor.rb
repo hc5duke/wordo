35b34b00a1dcceb01dd214a20b2fea9fcfb0b851
hc5duke <hc5duke@gmail.com>
2012-04-23 01:02:37 -0700

    training data creator

--- lib/recolor.rb
+++ lib/recolor.rb
@@ -2,40 +2,34 @@ require 'rmagick'
 
 color_white = Magick::Pixel.new(65535, 65535, 65535, 65535)
 color_black = Magick::Pixel.new(0, 0, 0, 65535)
-Dir['*.png'].each do |name|
+
+tile_size = 12
+bottom_corner_size = 5
+
+Dir['./train2/*.png'].each do |name|
   next if /black/ === name
-  black = Magick::Image.read("./#{name}").first
-  black.crop(0, 14, 24, 10).color_histogram
-  [1,2,3].zip([2,3,4]).map{|a,b|a*b}
-  min = black.export_pixels(0, 0, 24, 14, 'I').min
-  if min > 20_000
-    24.times.each do |col|
-      black.export_pixels(0, col, 24, 1, 'I').each_with_index do |pixel, index|
-        if pixel > 60_000
-          color = color_black
-        else
-          color = color_white
-        end
-        index += col * 24
-        co = index % 24
-        ro = index / 24
+  black = Magick::Image.read("./#{name}").first.resize(tile_size, tile_size)
+  black.crop(0, tile_size - bottom_corner_size, tile_size, bottom_corner_size).color_histogram
+  if black.export_pixels(0, 0, tile_size, tile_size - bottom_corner_size, 'I').min > 20_000
+    tile_size.times.each do |col|
+      black.export_pixels(0, col, tile_size, 1, 'I').each_with_index do |pixel, index|
+        color = pixel > 60_000 ? color_black : color_white
+        index += col * tile_size
+        co = index % tile_size
+        ro = index / tile_size
         black = black.color_point(co, ro, color)
       end
     end
   else
-    24.times.each do |col|
-      black.export_pixels(0, col, 24, 1, 'I').each_with_index do |pixel, index|
-        if pixel < 20_000
-          color = color_black
-        else
-          color = color_white
-        end
-        index += col * 24
-        co = index % 24
-        ro = index / 24
+    tile_size.times.each do |col|
+      black.export_pixels(0, col, tile_size, 1, 'I').each_with_index do |pixel, index|
+        color = pixel < 20_000 ? color_black : color_white
+        index += col * tile_size
+        co = index % tile_size
+        ro = index / tile_size
         black = black.color_point(co, ro, color)
       end
     end
   end
-  black.quantize(4, Magick::GRAYColorspace).write("./black_#{name}")
+  black.quantize(4, Magick::GRAYColorspace).write("./black_#{name.split('/').last}")
 end
